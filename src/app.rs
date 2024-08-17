78ef3ad50c52e27632bf6450f08122a86db3ce00
姜坤 <jiangkun@sanhemg.com>
2024-08-17 17:39:20 +0800

    2

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,12 @@
+use makepad_micro_serde::SerJson;
 use makepad_widgets::*;
+use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
+use btleplug::platform::Manager;
+
+use tokio::runtime::Runtime;
+use std::error::Error;
+use std::time::Duration;
+use tokio::time;
        
 live_design!{
     import makepad_widgets::base::*;
@@ -70,6 +78,20 @@ live_design!{
                         },
                         text: "Counter: 0"
                     }
+                    button_ble = <Button> {
+                        margin: {
+                            top: 10,
+                            left: 100,
+                        }
+                        text: "获取蓝牙"
+                        draw_text:{color:#f00}
+                    }
+                    label_ble = <Label> {
+                        draw_text: {
+                            color: #f
+                        },
+                        text: "蓝牙: 0"
+                    }
                 }
             }
         }
@@ -103,6 +125,7 @@ impl LiveRegister for App {
 
 impl MatchEvent for App{
     fn handle_actions(&mut self, cx: &mut Cx, actions:&Actions){
+        
         if self.ui.button(id!(button1)).clicked(&actions) {
             log!("BUTTON jk {}", self.counter); 
             self.counter += 1;
@@ -110,6 +133,83 @@ impl MatchEvent for App{
             label.set_text_and_redraw(cx,&format!("Counter: {}", self.counter));
             //log!("TOTAL : {}",TrackingHeap.total());
             
+        }
+        if self.ui.button(id!(button_ble)).clicked(&actions) {
+            log!("BUTTON ble {}", self.counter); 
+            let label = self.ui.label(id!(label_ble));
+            // let result = block_on(future);
+            let rt = Runtime::new().unwrap();
+            let fut = async move {
+                let manager = Manager::new().await.unwrap();
+                let adapter_list = manager.adapters().await.unwrap();
+                if adapter_list.is_empty() {
+                    println!("No Bluetooth adapters found");
+                    label.set_text_and_redraw(cx, "蓝牙连接失败");
+                } else {
+                    println!("蓝牙连接成功    : ==> {:?}", adapter_list);
+                    label.set_text_and_redraw(cx, "蓝牙连接成功");
+                    time::sleep(Duration::from_secs(2)).await;
+                    for adapter in adapter_list.iter() {
+                        println!("Starting scan on {}...", adapter.adapter_info().await.unwrap());
+                        adapter
+                            .start_scan(ScanFilter::default())
+                            .await
+                            .expect("Can't scan BLE adapter for connected devices...");
+                        time::sleep(Duration::from_secs(2)).await;
+                        let peripherals = adapter.peripherals().await.unwrap();
+                        if peripherals.is_empty() {
+                            eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+                        } else {
+                            // All peripheral devices in range
+                            for peripheral in peripherals.iter() {
+                                let properties = peripheral.properties().await.unwrap();
+                                let is_connected = peripheral.is_connected().await.unwrap();
+                                let local_name = properties
+                                    .unwrap()
+                                    .local_name
+                                    .unwrap_or(String::from("(peripheral name unknown)"));
+                                println!(
+                                    "Peripheral {:?} is connected: {:?}",
+                                    local_name, is_connected
+                                );
+                                if !is_connected {
+                                    println!("Connecting to peripheral {:?}...", &local_name);
+                                    if let Err(err) = peripheral.connect().await {
+                                        eprintln!("Error connecting to peripheral, skipping: {}", err);
+                                        continue;
+                                    }
+                                }
+                                let is_connected = peripheral.is_connected().await.unwrap();
+                                println!(
+                                    "Now connected ({:?}) to peripheral {:?}...",
+                                    is_connected, &local_name
+                                );
+                                peripheral.discover_services().await.unwrap();
+                                println!("Discover peripheral {:?} services...", &local_name);
+                                for service in peripheral.services() {
+                                    println!(
+                                        "Service UUID {}, primary: {}",
+                                        service.uuid, service.primary
+                                    );
+                                    for characteristic in service.characteristics {
+                                        println!("  {:?}", characteristic);
+                                    }
+                                }
+                                if is_connected {
+                                    println!("Disconnecting from peripheral {:?}...", &local_name);
+                                    peripheral
+                                        .disconnect()
+                                        .await
+                                        .expect("Error disconnecting from BLE peripheral");
+                                }
+                            }
+                        }
+                    }
+
+                }
+                // label.set_text_and_redraw(cx, &ds.join(";"));
+            };
+            rt.block_on(fut);
         }
     }
 }
